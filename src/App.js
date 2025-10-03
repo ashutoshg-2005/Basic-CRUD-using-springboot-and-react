a5adca5573263c1e3d011d7ec332a0179a84d2ee
Ashutosh Gaharwar <ashutoshgaharwar05@gmail.com>
2025-10-03 15:44:49 +0530

    Add user management features with routing and UI components
    - Implemented user listing, adding, editing, and viewing functionalities.
    - Integrated Axios for API calls to manage user data.
    - Added Bootstrap for styling and layout.
    - Created Navbar for navigation between different user-related pages.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./layout/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import AddUsers from "./users/AddUsers";
+import EditUser from "./users/EditUser";
+import ViewUser from "./users/ViewUser";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/adduser" element={<AddUsers />} />
+          <Route path="/edituser/:id" element={<EditUser />} />
+          <Route path="/viewuser/:id" element={<ViewUser />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
